a586093d291087b504ababf9f8a6ea5d53cedece
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-10-14 17:36:52 -0400

    improve client test to load root doc

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,9 +4,27 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe PMP::Client do
 
+  before(:each) {
+    @pmp = PMP::Client.new
+  }
+
   it "returns a root api object" do
-    pmp = PMP::Client.new
-    pmp.root.wont_be_nil
+    @pmp = PMP::Client.new
+    @pmp.root.wont_be_nil
+    @pmp.root.wont_be :loaded?
+    @pmp.root.href.must_equal "https://api.pmp.io/"
+  end
+
+  it "root doc can be loaded" do
+
+    root_doc =  json_file(:collection_root)
+
+    stub_request(:get, "https://api.pmp.io/").
+      with(:headers => {'Accept'=>'application/vnd.pmp.collection.doc+json', 'Content-Type'=>'application/vnd.pmp.collection.doc+json', 'Host'=>'api.pmp.io:443', 'User-Agent'=>'PMP Ruby Gem 0.0.1'}).
+      to_return(:status => 200, :body => root_doc, :headers => {})
+
+    @root = @pmp.root
+    @root.creator.must_be_instance_of PMP::Link
   end
 
 end
